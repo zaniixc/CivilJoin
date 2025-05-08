40b3a356cd374e6f8860c5b21575997c5a5c8cd7
zaniixc <zaniboi12@gmail.com>
2025-05-08 13:58:14 +0800

    Added important file(s)

--- src/main/java/com/example/civiljoin/HelloApplication.java
+++ src/main/java/com/example/civiljoin/HelloApplication.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600); // corrected the window size doon sa LoginPage.fxml
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
