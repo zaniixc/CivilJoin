0445277301611dbd54dee55481cb939ff67539ae
Alden Cedric <aldencedric314@gmail.com>
2025-05-12 16:31:58 +0800

    Changed all of the color names to Hex, set the title to CivilJoin

--- src/main/java/com/example/civiljoin/HelloApplication.java
+++ src/main/java/com/example/civiljoin/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 350);
-        stage.setTitle("Hello!");
+        stage.setTitle("CivilJoin");
         stage.setScene(scene);
         stage.show();
     }
